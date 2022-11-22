499fd16c073fbc512720912ef50735e4d9ce5b04
Aleksey Nesterenko <naa1205@bk.ru>
2022-11-22 22:59:17 +0700

    Homework

--- Homework1/Task1.py
+++ Homework1/Task1.py
@@ -3,3 +3,15 @@
 # - 6 -> да
 # - 7 -> да
 # - 1 -> нет
+
+#проба try - except 
+
+try:
+    list_of_days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
+    week_day = int(input('Введите номер дня недели от 1 до 7: \n'))
+    if week_day == 6 or week_day == 7:
+        print(f'{list_of_days[week_day-1]} - Ура, выходной :)')
+    else:
+        print(f'{list_of_days[week_day-1]} - Нужно работать :(')
+except Exception:
+    print('Нужно ввести номер дня недели от 1 до 7!')
--- Homework1/Task4.py
+++ Homework1/Task4.py
@@ -1,1 +1,18 @@
 #Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+#проба try - except 
+
+try:
+    quarter_number = int(input('Введите номер четверти координатной плоскости: \n'))
+    if quarter_number == 1:
+        print('x > 0 и y > 0')
+    elif quarter_number == 2:
+        print('x < 0 и y > 0')
+    elif quarter_number == 3:
+        print('x < 0 и y < 0')
+    elif quarter_number == 4:
+        print('x > 0 и y < 0')
+    else:
+        print('Это не номер четверти координатной плоскости!')
+except Exception:
+    print('Нужно ввести число!')
