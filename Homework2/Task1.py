fb352923e23a308f02d1950e84ae7e3a79bf5547
Aleksey Nesterenko <naa1205@bk.ru>
2022-11-26 16:20:02 +0700

    Homework2

--- Homework2/Task1.py
+++ Homework2/Task1.py
@@ -1,3 +1,13 @@
 # Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
 # Пример:
 # пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+
+num = int(input('Введите число: \n'))
+list =[]
+for i in range(1,num+1):
+    number_to_list = 1
+    for j in range(1,i+1):
+        number_to_list *= j
+    list.append(number_to_list)
+print(list)
--- Homework2/Task3.py
+++ Homework2/Task3.py
@@ -3,3 +3,21 @@
 # Пример списка ИНДЕКСОВ [2, 2, 3, 1, 8]
 # Ввод: 4
 # [-4, -3, -2, -1, 0, 1, 2, 3,4]
+
+num = int(input('Введите целое число: \n'))
+list = []
+for i in range(-num, num+1):
+    list.append(i)
+
+import random
+index_list  = []
+while len(index_list)<5:
+    index_list.append(random.randint(0,num * 2 + 1))
+
+product = 1
+for j in range(len(index_list)):
+    product *= list[index_list[j]]
+    
+print(index_list)
+print(list)
+print(product)
